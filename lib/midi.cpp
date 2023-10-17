1516a544a020a5a74be374345f33e6178bb0045f
Brenton Bostick <bostick@gmail.com>
2023-10-17 07:37:15 -0400

    cleanup

--- lib/midi.cpp
+++ lib/midi.cpp
@@ -52,8 +52,6 @@ computeChannelMap(
     channelMap.clear();
 
     //
-    // TabIt has 15 tracks
-    //
     // Channel 9 is for drums and is not generally available
     //
     std::vector<uint8_t> availableChannels{ 0, 1, 2, 3, 4, 5, 6, 7, 8, /*9,*/ 10, 11, 12, 13, 14, 15 };
